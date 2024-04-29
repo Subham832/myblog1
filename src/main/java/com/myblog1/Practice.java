ae3854180c8d7a3bf2b31ded4895bcf544a17bda
Subham Kumar <subhamkumar832@gmail.com>
2024-04-29 15:31:56 +0530

    Implemented 112: Create A Method And Call That Method

---
+++ src/main/java/com/myblog1/Practice.java
@@ -0,0 +1,14 @@
+package com.myblog1;
+
+public class Practice {
+    public static void main(String[] args) {
+        Practice p = new Practice();
+        int test1 = p.test();
+        System.out.println(test1);
+
+    }
+    public int test(){
+        int x = 10;
+        return x;
+    }
+}
